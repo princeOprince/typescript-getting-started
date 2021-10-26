7c747cc88f30e4921984402fa554b107c7808f21
Prince Odia <princeodia@gmail.com>
2021-10-26 09:56:25 +0100

    Add tasks on built-in types

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,7 +1,46 @@
 function startGame() {
   // starting a new game
-  var messagesElement = document.getElementById('messages');
+
+  let playerName: string = "Prince";
+  logPlayer(playerName);
+
+  let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+
+}
+
+// #region CONTROL FLOW BASED TYPE ANALYSIS
+function checkType(variable: string | number) {
+  if (typeof variable === 'string') {
+    console.log('typeof argument =', typeof variable);
+  }
+  else {
+    console.log('typeof argument =', typeof variable);
+  }
+  let check: string = "";
+  variable = check || 2;
+  console.log(variable);
+}
+//#endregion
+
+// #region NEVER TYPE
+function returnMe(variable: string) {
+  if (typeof variable === 'string') {
+    console.log('typeof argument =', typeof variable);
+    return variable;
+  }
+  else {
+    console.log('typeof argument =', typeof variable);
+    return variable;
+  }
+}
+// #endregion
+
+function logPlayer(name: string) {
+  console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+let a: string;    
+console.log(a!.length);
